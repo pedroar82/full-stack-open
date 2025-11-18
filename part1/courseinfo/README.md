ec21b1a3673ab22996d4775a628bcefc9dca8b58
Pedro Araújo <pedroprog@gmail.com>
2025-11-18 09:58:51 +0000

    Solution to Exercises 1.1 and 1.2 - Course Information

---
+++ part1/courseinfo/README.md
@@ -0,0 +1,3 @@
+# Exercises 1.1.-1.2
+
+This commit contains both exercises, i.e., it already contains the refactoring to the Content component.
---
+++ part1/courseinfo/src/Part.jsx
@@ -0,0 +1,13 @@
+
+
+const Part = (props) => {
+  return (
+    <div>
+        <p>
+        {props.part} {props.exercise}
+      </p>
+    </div>
+  )
+}
+
+export default Part
