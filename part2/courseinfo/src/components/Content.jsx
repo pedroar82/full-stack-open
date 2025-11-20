dc44405361375ea8472f7008108257063adbc6b5
Pedro Araújo <pedroprog@gmail.com>
2025-11-20 21:21:34 +0000

    Solution to Exercises 2.2 and 2.3: Course information steps 7 and 8

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,11 +1,14 @@
 import Part from './Part'
 
 const Content = (props) => {
+  console.log('parts ', props.parts)
   return (
     <div>
       {props.parts.map(note =>
         <Part name={note.name} exercise={note.exercises} key={note.id} />
       )}
+      <p><b>total of {props.parts.reduce((ac, course) =>
+        ac + course.exercises, 0)} exercises</b></p>
     </div>
   )
 }
