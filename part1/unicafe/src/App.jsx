4025a0b27a8c6fece45574613a992ea03d46b1bc
Pedro Araújo <pedroprog@gmail.com>
2025-11-20 08:29:39 +0000

    Solution to Exercises 1.11: unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 
 const StatisticLine = (props) => {
   return (
-    <div>
-      {props.text} {props.value}
-    </div>
+
+    <tr><td>{props.text} </td><td>{props.value}</td> </tr>
+
   )
 }
 
@@ -26,23 +26,23 @@ const Statistics = (props) => {
     return (
       <div>
         <h1>statistics</h1>
-        <p>
+        <table>
           <StatisticLine text="good" value={good} />
           <StatisticLine text="neutral" value={neutral} />
           <StatisticLine text="bad" value={bad} />
           <StatisticLine text="all" value={good + neutral + bad} />
           <StatisticLine text="average" value={calcAverage()} />
           <StatisticLine text="positive" value={calcPositive() + "%"} />
-        </p>
+        </table>
       </div>
     )
   }
   return (
-    <div>
-      <h1>statistics</h1>
-      <p>No feedback given</p>
-    </div>
+    <table>
+      <tr><td><h1>No feedback given</h1></td></tr>
+    </table>
   )
+
 }
 
 const Button = (props) => (
@@ -72,9 +72,12 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <Button onClick={handleGoodClick} text="good" />
-      <Button onClick={handleNeutralClick} text="neutral" />
-      <Button onClick={handleBadClick} text="bad" />
+      <table >
+        <tr><td><Button onClick={handleGoodClick} text="good" /></td>
+          <td><Button onClick={handleNeutralClick} text="neutral" /></td>
+          <td><Button onClick={handleBadClick} text="bad" /></td>
+        </tr>
+      </table>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
