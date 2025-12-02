67d8798050b57916b0740050e5758838592341d7
Pedro Araújo <pedroprog@gmail.com>
2025-12-02 12:35:13 +0000

    Solution to Exercise 3.8: Phonebook backend step 8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 var morgan = require('morgan')
 
-app.use(morgan('tiny'))
-
 app.use(express.json())
+
+morgan.token('body', req => JSON.stringify(req.body));
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body', {
+    skip: req => req.method !== 'POST'
+  })
+);
 
 let  persons = 
     [
