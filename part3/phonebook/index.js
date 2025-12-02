4be3836a7807548f636b130cdfad50db3aff65e4
Pedro Araújo <pedroprog@gmail.com>
2025-12-02 11:48:10 +0000

    Solution to Exercise 3.5: Phonebook backend step 5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let  persons = 
     [
         { 
@@ -60,3 +62,32 @@ app.delete('/api/persons/:id', (request, response) => {
 
   response.status(204).end()
 })
+
+//3.5: Phonebook backend step 5
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number ) {
+    return response.status(400).json({ 
+      error: 'incomplete person entry' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+ 
