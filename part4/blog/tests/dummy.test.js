4b7d75b27c6f5689e4f78c1572f6b041c4f76f61
Pedro Araújo <pedroprog@gmail.com>
2025-12-06 15:43:57 +0000

    Solution to Exercise 4.4 and 4.5: Lint Blog list, step 2 and 3

--- part4/blog/tests/dummy.test.js
+++ part4/blog/tests/dummy.test.js
@@ -2,9 +2,46 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+const blogs = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    __v: 0
+  },
+  {
+    title: 'A Guide to Express Middleware',
+    author: 'Mary Johnson',
+    url: 'https://example.com/express-middleware',
+    likes: 15
+  },
+  {
+    title: 'Async/Await Tips',
+    author: 'John Doe',
+    url: 'https://example.com/async-await',
+    likes: 99
+  }
+]
+
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 119)
+  })
+})
+
+describe('favorite Blog', () => {
+  test('favorite Blog is', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    assert.deepStrictEqual(result, blogs[2])
+  })
 })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,7 +1,22 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes  = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, item) => {
+      return sum + item.likes
+    }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((mostLikes, item) =>
+  {return mostLikes.likes >= item.likes ? mostLikes : item})
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
