2df027d4c51b04fd32d6eef4f856623e332b1d12
Pedro Araújo <pedroprog@gmail.com>
2025-12-09 10:40:54 +0000

    Solution to Exercise 4.1~5 to 4.19: Blog List Expansions, step 3 to 7

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,14 +1,35 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')){
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user')
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
 
-  const { title, author, url, likes } = request.body
+  if (!decodedToken.id){
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
+
+  const { title, author, url, likes } = body
 
   if (!title || !url) {
     return response.status(400).json({ error: 'title or url are missing' })
@@ -18,10 +39,13 @@ blogsRouter.post('/', async (request, response) => {
     title: title,
     author: author,
     url: url,
-    likes: likes || 0
+    likes: likes || 0,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
