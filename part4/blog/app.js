3e944aebcfa1a5acbbd17100b5b470d7b3fb3b70
Pedro Araújo <pedroprog@gmail.com>
2025-12-09 11:08:43 +0000

    Solution to Exercise 4.1~5 to 4.20: Blog List Expansions, step 8

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -23,6 +23,7 @@ mongoose
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
@@ -30,5 +31,6 @@ app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+
 
 module.exports = app
