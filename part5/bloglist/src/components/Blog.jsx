11648dc7402c290b77ee4bab18004cf76fc4f10d
Pedro Araújo <pedroprog@gmail.com>
2025-12-11 10:14:00 +0000

    Solution to Exercise 5.7: Blog List Fronted, step 7

--- part5/bloglist/src/components/Blog.jsx
+++ part5/bloglist/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetail, setShowDetail] = useState(false)
+
+  const showWhenVisible = { display: showDetail ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleView = event => {
+    setShowDetail(!showDetail)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={toggleView} >{ showDetail ? 'hide' : 'view'}</button>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>{blog.likes} <button>like</button></p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
