596b3cea182db56bf08f9589e168ef0ce4ba6404
Pedro Araújo <pedroprog@gmail.com>
2026-01-05 21:17:37 +0000

    Solution to Exercise 8.19: Books by genre, part 1

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react'
 import { useQuery } from '@apollo/client/react'
 import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
-  if (!props.show) {
-    return null
-  }
+  const [genre, setGenre] = useState('all')
 
   const result = useQuery(ALL_BOOKS)
 
   if (result.loading) {
     return <div>loading...</div>
+  }
+
+  const books = result.data.allBooks
+  const genres = [
+    ...new Set(
+      result.data.allBooks.reduce((genres, b) => genres.concat(b.genres), [])
+    ),
+  ]
+
+  const handleSetGenre = (selectedGenre) => {
+    setGenre(selectedGenre)
   }
 
   return (
@@ -23,15 +33,25 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {result.data.allBooks.map((a) => (
-            <tr key={a.id}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter((b) => (genre === 'all' ? true : b.genres.includes(genre)))
+            .map((a) => (
+              <tr key={a.id}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      {genres.map((g, index) => (
+        <button key={index} onClick={() => handleSetGenre(g)}>
+          {g}
+        </button>
+      ))}
+      <button key={genres.length + 1} onClick={() => handleSetGenre('all')}>
+        all
+      </button>
     </div>
   )
 }
