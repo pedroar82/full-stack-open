95870009690c86c193bbb531c065783a4a16691a
Pedro Araújo <pedroprog@gmail.com>
2026-01-08 22:25:42 +0000

    Solution to Exercise 9.3: Command line

--- part9/bmicalculator/exerciseCalculator.ts
+++ part9/bmicalculator/exerciseCalculator.ts
@@ -9,6 +9,28 @@ interface Exercise {
   average: number
 }
 
+interface CalculatorValues {
+  target: number
+  exerciseDays: number[]
+}
+
+const parseExerciseArguments = (args: string[]): CalculatorValues => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 12) throw new Error('Too many arguments')
+
+  let days = args.splice(3, args.length)
+
+  if (!isNaN(Number(args[2])) && days.every((n) => !isNaN(Number(n)))) {
+    return {
+      target: Number(args[2]),
+      exerciseDays: days.map((d) => Number(d)),
+    }
+  } else {
+    throw new Error('Provided values were not numbers!')
+  }
+}
+
+
 const calculateExercises = (
   exerciseHours: Array<number>,
   target: number
@@ -19,10 +41,11 @@ const calculateExercises = (
   let ratingDescription = 'Ronaldo, is that you?'
   let rating = 3
 
-  if (average < 1) {
+  //doing less hours than half of the target time per day
+  if (average < target/2) {
     rating = 1
-    ratingDescription = 'i know you can do better'
-  } else if (average >= 1 && average <= 3) {
+    ratingDescription = 'no pain, no gain'
+  } else if (average >= target/2 && average < target) {
     rating = 2
     ratingDescription = 'not too bad but could be better'
   }
@@ -36,8 +59,19 @@ const calculateExercises = (
     target: target,
     average: average,
   }
-
   return result
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+
+try {
+  const { target, exerciseDays } = parseExerciseArguments(process.argv)
+  console.log(calculateExercises(exerciseDays, target))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+  }
+  console.log(errorMessage)
+}
+
+//console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
