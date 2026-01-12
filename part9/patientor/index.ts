52802d9c63331a9f9d594ad085871b237c8553b0
Pedro Araújo <pedroprog@gmail.com>
2026-01-12 10:27:47 +0000

    Solution to Exercise 9.9: Patientor backend, step 2

--- part9/patientor/index.ts
+++ part9/patientor/index.ts
@@ -2,6 +2,10 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+const cors = require('cors')
+
+app.use(cors())
+
 const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
