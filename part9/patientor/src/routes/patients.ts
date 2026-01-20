d89456767549a2969541d3f1654a384472343acb
Pedro Araújo <pedroprog@gmail.com>
2026-01-20 18:27:53 +0000

    Solution to Exercise 9.14: Patientor backend, step 7

--- part9/patientor/src/routes/patients.ts
+++ part9/patientor/src/routes/patients.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import express, { Request, Response, NextFunction }  from 'express';
 import patientService from '../services/patients'
-import toNewPatient from '../utils';
+import { NewPatientSchema } from '../utils';
+import {Patient, NewPatient} from '../types'
+import { z } from 'zod';
 
 const router = express.Router();
 
@@ -8,19 +10,29 @@ router.get('/', (_req, res) => {
   res.send(patientService.getNonSensitivePatients());
 });
 
-
-router.post('/', (req, res) => {
+const newPatientParser = (req: Request, _res: Response, next: NextFunction) => { 
   try {
-    const newPatient = toNewPatient(req.body);
-    const addedPatient = patientService.addPatient(newPatient);
-    res.json(addedPatient);
+    NewPatientSchema.parse(req.body);
+    console.log(req.body);
+    next();
   } catch (error: unknown) {
-    let errorMessage = 'Something went wrong.';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    res.status(400).send(errorMessage);
+    next(error);
   }
-})
+};
+
+const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => { 
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
+
+router.post('/', newPatientParser, (req: Request<unknown, unknown, NewPatient>, res: Response<Patient>) => {
+  const addedPatient = patientService.addPatient(req.body);
+  res.json(addedPatient);
+});
+
+router.use(errorMiddleware);
 
 export default router;
