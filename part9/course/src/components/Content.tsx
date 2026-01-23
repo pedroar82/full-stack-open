4cf6b54dc8dc7c3d398c5cb4cfc0069ff533b689
Pedro Araújo <pedroprog@gmail.com>
2026-01-23 09:42:16 +0000

    Solution to Exercise 9.15: Course

---
+++ part9/course/src/components/Content.tsx
@@ -0,0 +1,25 @@
+interface CourseParts {
+  name: string;
+  exerciseCount: number;
+}
+
+interface ContentProps {
+  courseParts: CourseParts[];
+}
+
+const Content = (props: ContentProps) => {
+  const courseParts = props.courseParts;  
+  return <div>
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+  </div>;
+};
+
+export default Content;
