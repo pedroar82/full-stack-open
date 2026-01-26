030521c2bd145f5f5d6d35b5a31623b92239e5fe
Pedro Araújo <pedroprog@gmail.com>
2026-01-26 09:34:38 +0000

    Solution to Exercise 9.18: Flight Diary add entry

--- part9/flight-diary-front/src/App.tsx
+++ part9/flight-diary-front/src/App.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import axios from 'axios';
 
 const App = () => {
+  const url = 'http://localhost:3000/api/diaries';
   const [entries, setEntries] = useState<DiaryEntry[]>([
     {
       id: '1',
@@ -14,14 +15,70 @@ const App = () => {
   ]);
 
   useEffect(() => {
-    axios.get<DiaryEntry[]>('http://localhost:3000/api/diaries').then((response) => {
+    axios.get<DiaryEntry[]>(url).then((response) => {
       setEntries(response.data);
     })
-  }, [])
+  }, []);
+
+  const [newDate, setDate] = useState('');
+  const [newVisibility, setVisibility] = useState('');
+  const [newWeather, setWeather] = useState('');
+  const [newComment, setComment] = useState('');
+
+  const addEntry = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    const newEntry = {
+      date: newDate,
+      visibility: newVisibility,
+      weather: newWeather,
+      comment: newComment,
+    }
+
+    axios.post<DiaryEntry>(url, newEntry)
+      .then(response => {
+        setEntries(entries.concat(response.data))
+      })
+
+    setVisibility('');
+    setWeather('');
+    setComment('');
+    setDate('');
+  }
 
   return (
     <div>
-
+      <h1>Add new entry</h1>
+      <form onSubmit={addEntry}>
+        <div>
+          date
+          <input
+            value={newDate}
+            onChange={(event) => setDate(event.target.value)}
+          />
+        </div>
+        <div>
+          visibility
+          <input
+            value={newVisibility}
+            onChange={(event) => setVisibility(event.target.value)}
+          />
+        </div>
+        <div>
+          weather
+          <input
+            value={newWeather}
+            onChange={(event) => setWeather(event.target.value)}
+          />
+        </div>
+        <div>
+          comment
+          <input
+            value={newComment}
+            onChange={(event) => setComment(event.target.value)}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
       <h1>Diary entries</h1>
       {entries.map((entry) => (
         <div key={entry.id}>
